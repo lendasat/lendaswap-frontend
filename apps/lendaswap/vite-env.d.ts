b09f6c171c30996242f405586ff3b379a693b2ad
bonomat <philipp@hoenisch.at>
2025-10-29 15:03:03 +1100

    feat: also release a new frontend

--- apps/lendaswap/vite-env.d.ts
+++ apps/lendaswap/vite-env.d.ts
@@ -1,9 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_MEMPOOL_REST_URL?: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+    readonly env: ImportMetaEnv;
 }
