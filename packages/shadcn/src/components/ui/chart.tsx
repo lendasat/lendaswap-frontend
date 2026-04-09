69d13fd583ec854446b401b6ab4bd6e24f0f06f4
bonomat <philipp@hoenisch.at>
2026-04-09 15:31:13 +1000

    chore: bump frontend versions

--- packages/shadcn/src/components/ui/chart.tsx
+++ packages/shadcn/src/components/ui/chart.tsx
@@ -344,9 +344,9 @@ function getPayloadConfigFromPayload(
 
 export {
   ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
   ChartLegend,
   ChartLegendContent,
   ChartStyle,
+  ChartTooltip,
+  ChartTooltipContent,
 };
